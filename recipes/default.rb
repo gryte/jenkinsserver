e3a20e032cfa41e4685f659cc0ed47b52351efa5
Adam Linkous <alinkous@gmail.com>
2016-10-02 17:45:05 -0500

    install java
    * add inspec test
    * add package resource install java 1.7 openjdk

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# install wget
+package 'wget' do
+  action :install
+end
+
+# download nux-dextop rpm
+remote_file 'jenkinsrepo_download' do
+  not_if { ::File.exist?('/etc/yum.repos.d/jenkins.repo') }
+  path '/etc/yum.repos.d/jenkins.repo'
+  source 'http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo'
+end
+
+# import jenkins repo
+execute 'jenkinsrepo_import' do
+  command 'rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key'
+end
+
+# install jenkins
+package 'jenkins' do
+  action :install
+end
+
+# install java 1.7 OpenJDK
+package 'java-1.7.0-openjdk' do
+  action :install
+end
--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -3,17 +3,17 @@ describe package('wget') do
   it { should be_installed }
 end
 
-# jenkins.repo is installed
-describe package('jenkins.repo') do
-  it { should be_installed }
-end
-
 # jenkins.repo file exists
 describe file('/etc/yum.repos.d/jenkins.repo') do
   it { should be_file }
 end
 
-# jenkinsis installed
+# jenkins is installed
 describe package('jenkins') do
+  it { should be_installed }
+end
+
+# java 1.7 openjdk is installed
+describe package('java-1.7.0-openjdk') do
   it { should be_installed }
 end
