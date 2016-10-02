387ae31cee7c96c938a80055c37e0254dcc28844
Adam Linkous <alinkous@gmail.com>
2016-10-02 16:40:06 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# jenkinsserver
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'jenkinsserver'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures jenkinsserver'
+long_description 'Installs/Configures jenkinsserver'
+version '0.1.0'
